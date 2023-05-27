dde4c880105fe49fdbda71c4c6b0525f54779c60
Tu nombre <jessica.berrio2104@gmail.com>
2023-05-27 12:32:20 -0500

    counter exercise

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Ejercicio Angular';
+  public counter: number = 10;
+
+  increaseBy(value:number): void {
+    this.counter += 1;
+  }
+  decreaseBy(value:number): void {
+    this.counter -= 1;
+  }
+  resetCounter(value:number): void {
+    this.counter = 10;
+  }
 }
