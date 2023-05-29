6a3b3795c08b7b8c6bf26d3e51cfcc4e8b726d0e
Tu nombre <jessica.berrio2104@gmail.com>
2023-05-29 13:13:42 -0500

    angular modules

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/counter.module';
 
 import { AppComponent } from './app.component';
-import { counterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    counterComponent,
-    HeroComponent,
-    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 02-bases/src/app/counter/counter.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { counterComponent } from './components/counter.component';
+
+@NgModule({
+  declarations: [
+    counterComponent
+  ],
+  exports: [counterComponent],
+})
+
+export class CounterModule{
+
+}
