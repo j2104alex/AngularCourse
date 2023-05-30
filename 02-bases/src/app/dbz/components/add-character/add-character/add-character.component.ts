0599be9d3fcf605579e8a46074cd2d4759d54b2d
Tu nombre <jessica.berrio2104@gmail.com>
2023-05-30 11:11:53 -0500

    interfaces, small components @Input ng-For

---
+++ 02-bases/src/app/dbz/components/add-character/add-character/add-character.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  //Los componentes propios de dbz no necesitan la palabra app
+  selector: 'dbz-add-character',
+  templateUrl: './add-character.component.html',
+  styleUrls: ['./add-character.component.css']
+})
+export class AddCharacterComponent {
+
+}
--- 02-bases/src/app/dbz/pages/main-page-component.ts
+++ 02-bases/src/app/dbz/pages/main-page-component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
-  templateUrl: './main-page-component.html'
+  templateUrl: './main-page-component.html',
 })
-
 export class MainPageComponent {
-  constructor() { }
+  constructor() {}
 
+  public characters: Character[] = [
+    {
+      name: 'Krilin',
+      power: 1000,
+    },
+    {
+      name:'Gokú',
+      power:9500,
+    },
+    {
+      name:'Vegeta',
+      power:7500,
+    }
+  ];
 }
